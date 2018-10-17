569e999b54e6a4fb43e455faf7cc3f71395dee67
Nate <nathanestes@gmail.com>
2018-10-17 15:29:15 -0600

    update json

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
-import { Header, Segment, List, Grid } from 'semantic-ui-react';
+import { Header, Segment, List, Grid, Container, Image } from 'semantic-ui-react';
+
 
 class Home extends Component {
   render() {
     return (
       <div>
-        <Header as='h1' textAlign='center'>Nate's Journey</Header>
-        <Header as='h2' textAlign='center'>Learning React</Header>
-        
+        <Container fluid>
+          <Image src='/images/front_page.jpg' fluid size='large' alt='nature'/>
+          <Header as='h1' textAlign='center'>Nate's Journey</Header>
+          <Header as='h2' textAlign='center'>Learning React</Header>
+        </Container>
+
         <Segment>
           This is a segment.
           My plan is to host this on Heroku and improve it a little each week.
           As I improve it I will explain what I did and teach it to others.
         </Segment>
+        
 
         <Grid divided='vertically'>
           <Grid.Row columns={2}>
