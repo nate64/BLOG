a36e8eb2a2fdc9fff1664c3a6f4c73d38611fcef
Nate <nathanestes@gmail.com>
2018-11-10 10:56:04 -0700

    blog branch test

--- client/src/components/Blog.js
+++ client/src/components/Blog.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react'
 class Blog extends Component {
   render() {
     return (
-      <h1>This is the Blog Page</h1>
+      <>
+        <h1>This is the Blog Page</h1>
+        <h1>This is a test.</h1>
+      </>
     )
   }
 }
